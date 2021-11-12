faf9e72e44f28487421536910f89b7e386ca6598
JustMiracle <72463931+EsterMiracle@users.noreply.github.com>
2021-11-12 18:20:51 +0300

    Delete lesson_1-3.py

--- lesson_1-3.py
+++
@@ -1,8 +0,0 @@
-"""3. Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе."""
-
-word1 = b"attribute"
-word2 = "класс".encode('utf-8')
-word3 = "функция".encode('utf-8')
-word4 = b"type"
-
-print(word1, word2, word3, word4)
